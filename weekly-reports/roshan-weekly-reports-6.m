29b31495b422332041541c1f692a6b01b463d443
roshan-mohan <142958314+roshan-mohan@users.noreply.github.com>
2023-10-05 16:31:29 -0700

    roshan-weekly-reports-6.m

---
+++ weekly-reports/roshan-weekly-reports-6.m
@@ -0,0 +1,13 @@
+---
+# Report 1 - Week of 08/21/2023 #
+
+# Reflections #
+
+Trying to make a wind chime without tubes, using infrared time of flight sensors to take the place of the tubes in traditional wind chimes, we will create a chimeless wind chime. Our design augments the traditional wind chime by replacing the chimes with more visual, interactive feedback. We hope for this to be an interactive art piece. For this project, we will be proposing two stages. 
+
+
+Stage 1: Small chime
+The first stage is a small scale chime that is sized like a normal wind chime but without the chimes. Whenever the striker hits a sensor area, the sound will be produced from the speaker on top of the chime, and an LED array at the top which will deliver visual feedback. 
+
+Stage 2: BIG ROOM
+The second stage is a larger scale art installation. In this art installation, the participant will “become” the wind chime. A large tire, similar to those found in playgrounds, will hang from the ceiling at the center of the room. The participant will then sit on the tire and swing around the room, hitting certain sensor areas displayed via light sources that will then produce audio and provide a cool visualization around the room walls and floor via TouchDesigner. 
